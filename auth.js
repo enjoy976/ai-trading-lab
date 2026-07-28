405665290ae67e4963d5048fe80c5c4a55c1964d
enjoy976 <tugmgl@gmail.com>
2026-07-28 23:19:02 +0800

    Update XAUUSD premium card design

--- auth.js
+++ auth.js
@@ -1,17 +1,26 @@
-
 // ===============================
 // SUPABASE CONFIG
 // ===============================
 
+
 const SUPABASE_URL = "https://xkxcprkzubjwfglcrivg.supabase.co";
+
 
 const SUPABASE_KEY = "sb_publishable_7rRB4Trj5OfjENqzvpu_YQ_otz11jrh";
 
 
+
 const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+
+SUPABASE_URL,
+
+SUPABASE_KEY
+
 );
+
+
+
+
 
 
 
@@ -20,58 +29,88 @@ const supabaseClient = supabase.createClient(
 // SIGN UP
 // ===============================
 
+
 async function signupUser(){
 
 
-const email = document.getElementById("signupEmail").value;
 
-const password = document.getElementById("signupPassword").value;
+const email = 
+
+document.getElementById("signupEmail").value;
+
+
+
+const password = 
+
+document.getElementById("signupPassword").value;
+
+
 
 
 
 const {error} = await supabaseClient.auth.signUp({
 
-email: email,
 
-password: password
+email:email,
+
+
+password:password
+
 
 });
 
 
 
 
+
+
 if(error){
+
 
 document.getElementById("signupResult").innerHTML =
 
+
 "❌ ERROR: " + error.message;
 
+
 return;
+
 
 }
 
 
 
 
+
+
+
 document.getElementById("signupResult").innerHTML =
+
 
 `
 
 🟢 ACCOUNT CREATED
 
+
 <br><br>
+
 
 Имэйл баталгаажуулалтаа шалгана уу.
 
+
 <br><br>
 
+
 📩 Confirm Email линк дээр дарсны дараа Login хийнэ үү.
+
 
 `;
 
 
 
 }
+
+
 
 
 
@@ -83,25 +122,40 @@ document.getElementById("signupResult").innerHTML =
 // LOGIN
 // ===============================
 
+
 async function loginUser(){
 
 
 
-const email = document.getElementById("loginEmail").value;
+const email =
+
+document.getElementById("loginEmail").value;
 
 
-const password = document.getElementById("loginPassword").value;
+
+const password =
+
+document.getElementById("loginPassword").value;
+
+
 
 
 
 
 const {data,error} = await supabaseClient.auth.signInWithPassword({
 
-email: email,
 
-password: password
+
+email:email,
+
+
+
+password:password
+
+
 
 });
+
 
 
 
@@ -110,12 +164,17 @@ password: password
 if(error){
 
 
+
 document.getElementById("loginResult").innerHTML =
+
+
 
 "❌ LOGIN FAILED: " + error.message;
 
 
+
 return;
+
 
 }
 
@@ -123,36 +182,40 @@ return;
 
 
 
-// SAVE USER SESSION INFO
-
-localStorage.setItem(
-"userEmail",
-data.user.email
-);
-
-
 
 
 
 document.getElementById("loginResult").innerHTML =
 
+
+
 `
 
 🟢 ACCESS GRANTED
 
+
 <br><br>
+
 
 Welcome:
 
+
 <br>
+
 
 ${data.user.email}
 
+
 <br><br>
+
 
 Opening Dashboard...
 
+
 `;
+
+
+
 
 
 
@@ -164,7 +227,9 @@ setTimeout(()=>{
 window.location.href="dashboard.html";
 
 
-},1500);
+},1000);
+
+
 
 
 
@@ -175,20 +240,38 @@ window.location.href="dashboard.html";
 
 
 
+
+
+
 // ===============================
 // LOGOUT
 // ===============================
 
+
 async function logoutUser(){
 
 
-await supabaseClient.auth.signOut();
+
+const {error} = await supabaseClient.auth.signOut();
 
 
-localStorage.removeItem("userEmail");
+
+
+if(error){
+
+
+console.log(error);
+
+
+}
+
+
+
+
 
 
 window.location.href="login.html";
+
 
 
 }
